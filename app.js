7b817bbbc67cbe0833b086831d6b9941de7e4164
jkennedy3161 <kennedyjt88@gmail.com>
2016-10-16 08:52:15 -0700

    Phase 2 - Refactoring.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // app.js
+angular.module('ngTodos', [])
+.controller('todoController', function($scope) {
+	$scope.todos = [];
+
+	$scope.addTodo = function() {
+		$scope.todos.push($scope.newTodo);
+		$scope.newTodo = '';
+	}
+
+	$scope.removeTodo = function(index) {
+		$scope.todos.splice(index, 1);
+	}
+});
